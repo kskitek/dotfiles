17600daf06e4b9f3b7d6f7c6ee3befeb3995a5f4
Krzysztof Skitek <krzysztof.skitek@gmail.com>
2020-10-29 15:30:32 +0100

    fix: ktop should watch all namespaces

--- fish/functions/ktop.fish
+++ fish/functions/ktop.fish
@@ -3,19 +3,19 @@ function ktop
   set sortBy $argv[2]
   switch $sortBy
     case cpu
-      set sortBy 3
-    case mem
       set sortBy 4
+    case mem
+      set sortBy 5
     case '*'
       set -e sortBy
   end
 
-  set top (kubectl get pod -o wide | ag $node | cut -d' ' -f1 | tr '\n' '|')
+  set top (kubectl get pod -o wide --all-namespaces | ag $node | cut -d' ' -f1 | tr '\n' '|')
   set top (echo '"'$top'NAME"')
 
   if test -z sortBy
-    kubectl top pod --containers=true | ag --nocolor $top
+    kubectl top pod --containers=true --all-namespaces | ag --nocolor $top
   else
-    kubectl top pod --containers=true | ag --nocolor $top | sort -h -k$sortBy
+    kubectl top pod --containers=true --all-namespaces | ag --nocolor $top | sort -h -k$sortBy
   end
 end
