28eba67a4374c84df9d36bf3ba2c6a1a40726e58
Krzysztof Skitek <krzysztof.skitek@nobl9.com>
2021-05-11 11:30:01 +0200

    wm: Improve lock, notifications, add ~/.x* files

--- autostart.sh
+++ autostart.sh
@@ -6,11 +6,19 @@ xset r rate 200 15
 # wal -R &
 # wal -i .dotfiles/background/current.jpg --saturate 0.6 -n -b 000000
 
-nitrogen --restore &
+# nitrogen --restore &
+# feh --bg-scale .dotfiles/background/current.jpg
+~/.fehbg &
+
+setxkbmap pl &
+
 compton &
 dunst -conf ~/.dotfiles/dunstrc &
-~/.dotfiles/bar/bar.sh &
-~/.dropbox-dist/dropboxd &
+
+# ~/.dotfiles/bar/bar.sh &
+# ~/.dropbox-dist/dropboxd &
+
+sleep 10 && /opt/Drata\ Agent/drata-agent &
 
 eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
